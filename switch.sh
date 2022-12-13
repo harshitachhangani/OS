be9c5fa8a0f90cd59a6b38408387fbedb783707b
Harshita Chhangani <harshitachhangani05@gmail.com>
2022-12-13 18:19:00 +0000

    swith added

--- switch.sh
+++ switch.sh
@@ -1,36 +1,32 @@
-a=$(shuf -i 0-10 -n1)
-
+a=$(shuf -i 0-50 -n1)
+echo $a
 n=1
-
-while [ $n -le 5]
+while [ $n -le 5 ]
 do 
-	echo "Enter Your Guess"
-	read guess
-
-	if [$n == $guess ]
-	then
-		chance = $n
-
-		case $chance in
-			"1") echo "You Won"
+	echo "Guess The No.:"
+	read ans
+		if [ $ans = $a ]
+		then
+			GUESS=$n
+			case $GUESS in
+				'1') echo "You won 1st prize"
 				;;
-			"2") echo "You Lare 2nd"
+				'2') echo "You won 2nd prize"
 				;;
-			"3") echo "You are 3rd"
+				'3') echo "You won 3rd prize"
 				;;
-			"4") echo "You are 4th"
+				'4') echo "You won 4th prize"
 				;;
-			"5") echo "You are 5th"
+				'5') echo "You won 5th prize"
 				;;
-			
-		esac
-	else
-		echo "Try again"
-
+			esac
+			break
+		else
+			echo "Try Again..."
+		fi
 	((n=n+1))
-
-	if [ $n == 6 ]
-	then 
-		echo "Better Luck Next Time"
-	fi 
+	if [ $n = 6 ]
+	then
+		echo "Better Luck Next Time :( "
+	fi
 done
